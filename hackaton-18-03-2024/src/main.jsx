ec049ae830542da1584aaf980bb28afd974d1cfa
pablitoodt <pablodeteba@icloud.com>
2024-03-18 17:28:32 +0100

    majPablo

--- hackaton-18-03-2024/src/main.jsx
+++ hackaton-18-03-2024/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import EnergyInfo from './components/Energy-info.jsx'
+import House from './components/House.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <EnergyInfo/>
+    <House/>
   </React.StrictMode>,
 )
