24779b2088fa561cbd8afdc28db377b5be9db91a
neyzar <lucas.narguet451@gmail.com>
2024-03-18 17:26:31 +0100

    push

--- hackaton-18-03-2024/src/main.jsx
+++ hackaton-18-03-2024/src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-<<<<<<< Updated upstream
 import EnergyInfo from './components/Energy-info.jsx'
-=======
-import House from './components/House.jsx'
->>>>>>> Stashed changes
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
