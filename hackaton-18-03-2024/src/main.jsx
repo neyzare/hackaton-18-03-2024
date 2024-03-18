b33aa9d546302894ada6c317bf2de903e85a7331
pablitoodt <pablodeteba@icloud.com>
2024-03-18 17:22:36 +0100

    pablo

--- hackaton-18-03-2024/src/main.jsx
+++ hackaton-18-03-2024/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
+<<<<<<< Updated upstream
 import EnergyInfo from './components/Energy-info.jsx'
+=======
+import House from './components/House.jsx'
+>>>>>>> Stashed changes
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
