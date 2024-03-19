1768dfb1b389cd65ba029375da98e41e91d0d770
pablitoodt <pablodeteba@icloud.com>
2024-03-19 11:00:33 +0100

    maj

--- hackaton-18-03-2024/src/main.jsx
+++ hackaton-18-03-2024/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css'
 import Banderoll from './components/Banderoll.jsx'
+import Card from '../src/components/Card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Banderoll />
     <App />
+    <Card />
   </React.StrictMode>,
 )
