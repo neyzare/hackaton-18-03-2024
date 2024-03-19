e9bb6ed9e4fc6358aaa77215a8c90fa2575c61da
neyzar <lucas.narguet451@gmail.com>
2024-03-19 16:17:09 +0100

    maj

--- hackaton-18-03-2024/src/components/Dpe.jsx
+++ hackaton-18-03-2024/src/components/Dpe.jsx
@@ -8,16 +8,16 @@ function Dpe() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://dpe-api-service-dev-xfyprtzkyq-ew.a.run.app/calcul_dpe/2362E0868302S', {
+        const response = await axios.get('https://dpe-api-service-dev-xfyprtzkyq-ew.a.run.app/trouve_info/2362E0868302S', {
           headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb2RhX3NjaG9vbF9zdHVkZW50IiwiZXhwIjoxNzEwODU0MzQ4fQ.uydgWmcOqUNOEKr6dLHjWxuhjPyDrcb1UzJVkLv2RDM'
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb2RhX3NjaG9vbF9zdHVkZW50IiwiZXhwIjoxNzEwODYxMzc3fQ.-Z8hy3HDNF2YK8HDC_J3tLmuP2ROyj6gqNNthPh6TUs'
           }
         });
         setDpeData(response.data);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching DPE data:', error);
+        console.error('Erreur lors de la récupération des données DPE :', error);
         setLoading(false);
       }
     };
@@ -26,7 +26,7 @@ function Dpe() {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Chargement en cours...</div>;
   }
 
   return (
@@ -34,14 +34,20 @@ function Dpe() {
       {dpeData && (
         <div>
           <h2>Message: {dpeData.Message}</h2>
-          <h3>Scenarios:</h3>
+          <h3>Détails du DPE :</h3>
           <ul>
-            {dpeData.Scenarios.map((scenario, index) => (
-              <li key={index}>
-                <p>Ancienne note DPE: {scenario.Ancienne_note}</p>
-                <p>Nouvelle note DPE: {scenario.Nouvelle_note}</p>
-              </li>
-            ))}
+            <li>
+              <p>Perdition de renouvellement d'air : {dpeData.deperdition_renouvellement_air}</p>
+              <p>Perdition des murs : {dpeData.deperdition_mur}</p>
+              <p>Perdition du plancher bas : {dpeData.deperdition_plancher_bas}</p>
+              <p>Perdition du plancher haut : {dpeData.deperdition_plancher_haut}</p>
+              <p>Perdition du pont thermique : {dpeData.deperdition_pont_thermique}</p>
+              <p>Perdition des menuiseries : {dpeData.deperdition_menuiserie}</p>
+              <p>Score DPE : {dpeData.score_dpe}</p>
+              <p>Classe DPE : {dpeData.classe_dpe}</p>
+              <p>Score GES : {dpeData.score_ges}</p>
+              <p>Classe GES : {dpeData.classe_ges}</p>
+            </li>
           </ul>
         </div>
       )}
