4eafd2302203ae5f01a65ac6bbe392b6afc32e8a
pablitoodt <pablodeteba@icloud.com>
2024-03-19 12:02:34 +0100

    maj hover de la house

--- hackaton-18-03-2024/src/components/Banderoll.jsx
+++ hackaton-18-03-2024/src/components/Banderoll.jsx
@@ -4,26 +4,7 @@ import '../css/Banderoll.css';
 function Banderoll() {
   const [isHovered, setIsHovered] = useState(false);
 
-  useEffect(() => {
-    const handleMouseMove = () => {
-      const bnd = document.querySelector("#bande");
-      bnd.style.width = "300px";
-    };
-
-    const handleMouseLeave = () => {
-      const bnd = document.querySelector("#bande");
-      bnd.style.width = "fit-content";
-    };
-
-    const bnd = document.querySelector("#bande");
-    bnd.addEventListener('mouseenter', handleMouseMove);
-    bnd.addEventListener('mouseleave', handleMouseLeave);
-
-    return () => {
-      bnd.removeEventListener('mouseenter', handleMouseMove);
-      bnd.removeEventListener('mouseleave', handleMouseLeave);
-    };
-  }, []);
+  
 
   return (
     <div className='bandeBox'>
